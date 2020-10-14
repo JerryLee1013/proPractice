3ec9f69203a717f0ac6995a4b79d412d172ef807
JerryLee <lifengxi1013@163.com>
2020-10-14 17:24:51 +0800

    项目基本配置和页面基本框架的搭建

---
+++ gshop/src/router/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import MSite from '../pages/MSite/MSite'
+import Search from '../pages/Search/Search'
+import Order from '../pages/Order/Order'
+import Profile from '../pages/Profile/Profile'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {path: '/msite', component: MSite},
+  {path: '/search', component: Search},
+  {path: '/order', component: Order},
+  {path: '/profile', component: Profile}
+]
+
+// eslint-disable-next-line
+export default new VueRouter({
+  routes
+})
